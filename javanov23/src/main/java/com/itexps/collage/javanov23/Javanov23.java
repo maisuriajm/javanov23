f971a6d6b7078c845fcc0c85a9baade9cca0feb9
maisuriajm <maisuriajm@yahoo.co.in>
2024-11-27 19:08:11 -0800

    Initial file

---
+++ javanov23/src/main/java/com/itexps/collage/javanov23/Javanov23.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itexps.collage.javanov23;
+
+/**
+ *
+ * @author maisu
+ */
+public class Javanov23 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
